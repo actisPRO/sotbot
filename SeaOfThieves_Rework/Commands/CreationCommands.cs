4792b4701b4fb7ce4ae1b34e193d21f85b4e3d88
Denis Karev <dkarev@protonmail.com>
2019-04-24 12:50:41 +0500

    Исправление бага, из-за которого бот не отвечал ошибкой, если пользователь пытался создать корабль командой, не находясь при этом в голосовом канале.

--- SeaOfThieves_Rework/Commands/CreationCommands.cs
+++ SeaOfThieves_Rework/Commands/CreationCommands.cs
@@ -10,62 +10,70 @@ namespace SeaOfThieves.Commands
     {
         [Command("create"), Aliases("c")]
         [Description("Создаёт новый корабль. Вы должны быть в голосовом канале, чтобы использовать это.")]
-        public async Task Create(CommandContext ctx, [Description("Количество членов экипажа (от 2 до 4)")] int slots = 4)
+        public async Task Create(CommandContext ctx, [Description("Количество членов экипажа (от 2 до 4)")]
+            int slots = 4)
         {
-            if (ctx.Member.VoiceState.Channel != null)
+            try
             {
-                if (Bot.ShipCooldowns.ContainsKey(ctx.User))
-                {   
-                    if ((Bot.ShipCooldowns[ctx.User] - DateTime.Now).Seconds > 0)
-                    {
-                        var m = await ctx.Guild.GetMemberAsync(ctx.User.Id);
-                        await m.PlaceInAsync(ctx.Guild.GetChannel(Bot.BotSettings.WaitingRoom));
-                        await ctx.RespondAsync($"{Bot.BotSettings.ErrorEmoji} Вам нужно подождать " +
-                                                 $"**{(Bot.ShipCooldowns[ctx.User] - DateTime.Now).Seconds}** секунд прежде чем " +
-                                                 $"создавать новый корабль!");
-                        return;
-                    }
-                }
-                Bot.ShipCooldowns[ctx.User] = DateTime.Now.AddSeconds(Bot.BotSettings.FastCooldown);
-                
-                if (slots < 2 || slots > 4)
-                {
-                    await ctx.RespondAsync(
-                        $"{Bot.BotSettings.ErrorEmoji} Количество членов экипажа должно быть от 2 до 4. ");
-                    return;
-                }
-
-                string name = "";
-                switch (slots)
-                {
-                    case 2:
-                        name = "Шлюп";
-                        break;
-                    case 3:
-                        name = "Бриг";
-                        break;
-                    case 4:
-                        name = "Галеон";
-                        break;
-                }
-
-                var created = await ctx.Guild.CreateChannelAsync($"{Bot.BotSettings.AutocreateSymbol} {name} {ctx.User.Username}",
-                    ChannelType.Voice, ctx.Guild.GetChannel(Bot.BotSettings.AutocreateCategory),
-                    Bot.BotSettings.Bitrate, slots);
-
-                await ctx.Member.PlaceInAsync(created);
-
-                await ctx.RespondAsync($"{Bot.BotSettings.OkEmoji} Успешно создан канал **{created.Name}**!");
-
-                ctx.Client.DebugLogger.LogMessage(LogLevel.Info, "SoT",
-                    $"{ctx.User.Username}#{ctx.User.Discriminator} created channel via command",
-                    DateTime.Now);
+                var channel =
+                    ctx.Member.VoiceState
+                        .Channel; //если здесь возникнет NullReferenceException, значит пользователь не находится в голосовом канале
             }
-            else
+            catch (NullReferenceException)
             {
                 await ctx.RespondAsync(
                     $"{Bot.BotSettings.ErrorEmoji} Вы должны быть в голосовом канале, чтобы использовать это.");
+                return;
             }
+
+            if (Bot.ShipCooldowns.ContainsKey(ctx.User))
+            {
+                if ((Bot.ShipCooldowns[ctx.User] - DateTime.Now).Seconds > 0)
+                {
+                    var m = await ctx.Guild.GetMemberAsync(ctx.User.Id);
+                    await m.PlaceInAsync(ctx.Guild.GetChannel(Bot.BotSettings.WaitingRoom));
+                    await ctx.RespondAsync($"{Bot.BotSettings.ErrorEmoji} Вам нужно подождать " +
+                                           $"**{(Bot.ShipCooldowns[ctx.User] - DateTime.Now).Seconds}** секунд прежде чем " +
+                                           $"создавать новый корабль!");
+                    return;
+                }
+            }
+
+            Bot.ShipCooldowns[ctx.User] = DateTime.Now.AddSeconds(Bot.BotSettings.FastCooldown);
+
+            if (slots < 2 || slots > 4)
+            {
+                await ctx.RespondAsync(
+                    $"{Bot.BotSettings.ErrorEmoji} Количество членов экипажа должно быть от 2 до 4. ");
+                return;
+            }
+
+            string name = "";
+            switch (slots)
+            {
+                case 2:
+                    name = "Шлюп";
+                    break;
+                case 3:
+                    name = "Бриг";
+                    break;
+                case 4:
+                    name = "Галеон";
+                    break;
+            }
+
+            var created = await ctx.Guild.CreateChannelAsync(
+                $"{Bot.BotSettings.AutocreateSymbol} {name} {ctx.User.Username}",
+                ChannelType.Voice, ctx.Guild.GetChannel(Bot.BotSettings.AutocreateCategory),
+                Bot.BotSettings.Bitrate, slots);
+
+            await ctx.Member.PlaceInAsync(created);
+
+            await ctx.RespondAsync($"{Bot.BotSettings.OkEmoji} Успешно создан канал **{created.Name}**!");
+
+            ctx.Client.DebugLogger.LogMessage(LogLevel.Info, "SoT",
+                $"{ctx.User.Username}#{ctx.User.Discriminator} created channel via command",
+                DateTime.Now);
         }
     }
 }
