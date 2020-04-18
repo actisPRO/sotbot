94956dd833acf2bd81e3fcd2883a950428bb5ecf
Denis Karev <dkarev@protonmail.com>
2020-04-18 19:05:58 +0200

    2.11.0 - KeyNotFoundException fix in !yes

--- Bot_NetCore/Commands/PrivateCommands.cs
+++ Bot_NetCore/Commands/PrivateCommands.cs
@@ -230,7 +230,18 @@ namespace SeaOfThieves.Commands
         public async Task Yes(CommandContext ctx, [Description("Корабль")] [RemainingText]
             string name)
         {
-            var ship = ShipList.Ships[name];
+            Ship ship = null;
+            try
+            {
+                ship = ShipList.Ships[name];
+            }
+            catch (KeyNotFoundException)
+            {
+                await ctx.RespondAsync(
+                    $"{Bot.BotSettings.ErrorEmoji} Корабль **{name}** не был найден! Проверьте правильность названия и попробуйте снова!");
+                return;
+            }
+            
             if (!ship.IsInvited(ctx.Member.Id))
             {
                 await ctx.RespondAsync(
