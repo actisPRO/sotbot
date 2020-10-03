49afee17e970a2b3315b56ed2d094aa873ee1b98
Denis Karev <dkarev@protonmail.com>
2020-10-03 06:01:23 +0200

    WarnSQL class, Create, Delete, Get functions

---
+++ Bot_NetCore/Entities/WarnSQL.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Bot_NetCore.Entities
+{
+    public class WarnSQL
+    {
+        public readonly string Id;
+
+        private ulong _user;
+        private ulong _moderator;
+        private string _reason;
+        private DateTime _date;
+        private ulong _logMessage;
+
+        public ulong User
+        {
+            get => _user;
+            set => _user = value;
+        }
+
+        public ulong Moderator
+        {
+            get => _moderator;
+            set => _moderator = value;
+        }
+
+        public string Reason
+        {
+            get => _reason;
+            set => _reason = value;
+        }
+        
+        public DateTime Date
+        {
+            get => _date;
+            set => _date = value;
+        }
+
+        public ulong LogMessage
+        {
+            get => _logMessage;
+            set => _logMessage = value;
+        }
+
+        private WarnSQL(string id, ulong user, ulong moderator, string reason, DateTime date, ulong logMessage)
+        {
+            Id = id;
+            User = user;
+            Moderator = moderator;
+            Reason = reason;
+            Date = date;
+            LogMessage = logMessage;
+        }
+
+        /// <summary>
+        /// Создаёт предупреждение с заданными параметрами
+        /// </summary>
+        public static WarnSQL Create(MySqlConnection connection, string id, ulong user, ulong moderator, string reason,
+            DateTime date, ulong logMessage)
+        {
+            var statement = $"INSERT INTO warnings(id, user, moderator, reason, date, logmessage) " +
+                            $"VALUES ('{id}', {user}, {moderator}, '{reason}', '{date:yyyy-mm-ddhh:mm:ss:ff}', {logMessage});";
+            var cmd = new MySqlCommand(statement);
+
+            cmd.ExecuteNonQuery();
+            
+            return new WarnSQL(id, user, moderator, reason, date, logMessage);
+        }
+
+        /// <summary>
+        /// Получает предупреждение из базы данных по заданному Id
+        /// </summary>
+        /// <param name="connection">Соединение с БД, которое обязательно должно быть открыто</param>
+        /// <param name="id">ID предупреждения</param>
+        /// <returns>Предупреждение или null, если не существует</returns>
+        public static WarnSQL Get(MySqlConnection connection, string id)
+        {
+            var statement = $"SELECT * FROM warnings WHERE id='{id}';";
+            var cmd = new MySqlCommand(statement, connection);
+            var reader = cmd.ExecuteReader();
+
+            // отсутствуют предупреждения с указанным ID
+            if (!reader.Read()) 
+            {
+                reader.Close();
+                return null;
+            }
+            else
+            {
+                var ret = new WarnSQL(reader.GetString("id"), reader.GetUInt64("user"), reader.GetUInt64("moderator"),
+                    reader.GetString("reason"), reader.GetDateTime("date"), reader.GetUInt64("logmessage"));
+                reader.Close();
+                return ret;
+            }
+        }
+        
+        /// <summary>
+        /// Удаляет предупреждение с заданным Id
+        /// </summary>
+        public static void Delete(MySqlConnection connection, string id)
+        {
+            var statement = $"DELETE FROM warnings WHERE id = '{id}';";
+            var cmd = new MySqlCommand(statement);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
