3dbd9932e4e7797c779e4ffd9df1db6181bf3240
Denis Karev <dkarev@protonmail.com>
2020-12-22 00:09:15 +0100

    StatsBot

---
+++ StatsBot/Bot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+
+namespace StatsBot
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension CommandsExtension { get; private set; }
+        
+        public static void Main(string[] args)
+        {
+            var bot = new Bot();
+        }
+
+        public async Task RunBotAsync()
+        {
+            var token = "";
+            using (var sr = new StreamReader("token.txt"))
+                token = await sr.ReadToEndAsync();
+            
+            var cfg = new DiscordConfiguration()
+            {
+                Token = token,
+                AutoReconnect = true,
+                TokenType = TokenType.Bot
+            };
+            Client = new DiscordClient(cfg);
+
+            var ccfg = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!s " },
+                EnableDms = false,
+                CaseSensitive = false,
+                EnableMentionPrefix = true
+            };
+            CommandsExtension = Client.UseCommandsNext(ccfg);
+            CommandsExtension.RegisterCommands<Commands>();
+
+            Client.Ready += (sender, args) =>
+            {
+                sender.Logger.LogInformation("Stats", "Bot is ready.");
+                return Task.CompletedTask;
+            };
+
+            await Task.Delay(-1);
+        }
+    }
+}
