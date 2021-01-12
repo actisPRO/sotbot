a6bfdb67e0ced7c9d0df8b921f74680956001079
Vanguard-Fx <nazarhnatyshyn@hotmail.com>
2021-01-12 17:09:26 +0100

    voice time store fix

--- Bot_NetCore/Entities/VoiceTimeSQL.cs
+++ Bot_NetCore/Entities/VoiceTimeSQL.cs
@@ -11,14 +11,14 @@ namespace Bot_NetCore.Entities
             {
                 using (var cmd = new MySqlCommand())
                 {
-                    cmd.CommandText = $"SELECT time FROM voice_times WHERE user_id = {userId}";
+                    cmd.CommandText = $"SELECT time_seconds FROM voice_times WHERE user_id = {userId}";
                     cmd.Connection = connection;
                     cmd.Connection.Open();
 
                     var reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        return reader.GetTimeSpan("time");
+                        return TimeSpan.FromSeconds(reader.GetInt64("time_seconds"));
                     }
                     return TimeSpan.Zero;
                 }
@@ -31,8 +31,8 @@ namespace Bot_NetCore.Entities
             {
                 using (var cmd = new MySqlCommand())
                 {
-                    cmd.CommandText = $"INSERT INTO voice_times(user_id, time) VALUES('{userId}', '{time}')" +
-                                      $"ON DUPLICATE KEY UPDATE time = ADDTIME(time,'{time}')";
+                    cmd.CommandText = $"INSERT INTO voice_times(user_id, time_seconds) VALUES('{userId}', '{(long)time.TotalSeconds}')\n" +
+                                      $"ON DUPLICATE KEY UPDATE time_seconds = time_seconds + '{(long)time.TotalSeconds}'";
                     cmd.Connection = connection;
                     cmd.Connection.Open();
 
