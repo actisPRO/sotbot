9b71bad0fbb5a4cc16831e0a764b629709a5c5ac
Denis Karev <dkarev@protonmail.com>
2021-01-28 15:47:28 +0100

    classes for private ship and private ship member mysql implementation

---
+++ Bot_NetCore/Entities/PrivateShip.cs
@@ -0,0 +1,34 @@
+﻿namespace Bot_NetCore.Entities
+{
+    public class PrivateShip
+    {
+        private string _name;
+        private ulong _channel;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                // mysql logic here
+                _name = value;
+            }
+        }
+
+        public ulong Channel
+        {
+            get => _channel;
+            set
+            {
+                // mysql logic here
+                _channel = value;
+            }
+        }
+
+        private PrivateShip(string name, ulong channel)
+        {
+            _name = name;
+            _channel = channel;
+        }
+    }
+}
---
+++ Bot_NetCore/Entities/PrivateShipMember.cs
@@ -0,0 +1,46 @@
+﻿namespace Bot_NetCore.Entities
+{
+    public class PrivateShipMember
+    {
+        public readonly string Ship;
+        public readonly ulong MemberId;
+
+        private PrivateShipMemberRole _role;
+        private bool _status;
+
+        public PrivateShipMemberRole Role
+        {
+            get => _role;
+            set
+            {
+                // mysql logic here
+                _role = value;
+            }
+        }
+
+        public bool Status
+        {
+            get => _status;
+            set
+            {
+                // mysql logic here
+                _status = value;
+            }
+        }
+
+        private PrivateShipMember(string ship, ulong memberId, PrivateShipMemberRole role, bool status)
+        {
+            Ship = ship;
+            MemberId = memberId;
+            Role = role;
+            Status = status;
+        }
+    }
+
+    public enum PrivateShipMemberRole
+    {
+        Member,
+        Officer,
+        Captain
+    }
+}
